48a6b3177a49a593c0655196c929bd4967ac6a62
lucca-sa <scherer.lucca@gmail.com>
2023-12-18 22:42:21 -0300

    Project Start!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'task-board',
+        pathMatch: "full"
+    },
+    {
+        path: 'task-board',
+        loadComponent: () => import('./components/task-board/task-board.component')
+    }
+];
---
+++ src/app/shared/services/task.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Task } from '../interfaces/Task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  
+
+  constructor(private http: HttpClient) { }
+
+  getAllTasks() {
+    return this.http.get<Task[]>(`${environment.baseApiUrl}/tasks`)
+  }
+
+  changeTaskStatusById(taskId: number, taskStatus: string): Observable<any> {
+    const url = `${environment.baseApiUrl}/tasks/${taskId}`;
+    const body = { status: `${taskStatus}` };
+
+    return this.http.patch(url, body);
+  }
+}
